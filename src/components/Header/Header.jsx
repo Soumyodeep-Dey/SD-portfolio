68af8d6c8de8fe08932ffaa3e3954a950ce0d6e4
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2024-10-31 00:47:53 +0530

    blog folder is added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,6 +30,18 @@ function Header() {
                 Projects
               </NavLink>
             </li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? 'text-orange-700' : 'text-gray-500'
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Blogs
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import Projects from './components/Projects/Projects.jsx'
+import Blogs from './components/Blog/Blogs.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
       <Route path="projects" element={<Projects />} />
+      <Route path="Blogs" element={<Blogs />} />
     </Route>
   )
 )
