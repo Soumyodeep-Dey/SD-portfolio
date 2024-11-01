7ddd93c53980d27b382677f2980f01b02498bbd4
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2024-11-02 00:01:58 +0530

    LinkedIn blog added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -7,19 +7,45 @@ function Blogs() {
       <div className="container mx-auto">
         <div className="max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden dark:bg-gray-800">
           <img
-            src="/path/to/your/thumbnail.jpg" // Replace with your actual thumbnail path
+            src="/public/Images/Add a heading.png" 
             alt="Blog Thumbnail"
-            className="w-full h-48 object-cover"
+            className="w-full h-full object-cover"
           />
           <div className="p-6">
-            <h2 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-gray-200">
-              Your Blog Title Here
+            <h2 className="text-2xl font-bold mb-2 text-gray-800 dark:text-gray-200">
+            A Beginners Guide to LinkedIn
             </h2>
             <p className="text-gray-700 dark:text-gray-300 mb-4">
-              A brief summary or excerpt of your blog goes here. This will give readers an idea of what to expect.
+            Effective Strategies for Expanding Your LinkedIn Network (most useful for Beginners)
             </p>
             <a
               href="https://hashnode.com/edit/cm02l0eje000408mhbmv61h6f"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-indigo-600 dark:text-indigo-400 font-semibold hover:underline"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container mx-auto">
+        <div className="max-w-2xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden dark:bg-gray-800">
+          <img
+            src="" 
+            alt="Blog Thumbnail"
+            className="w-full h-full object-cover"
+          />
+          <div className="p-6">
+            <h2 className="text-2xl font-bold mb-2 text-gray-800 dark:text-gray-200">
+            More on the way...
+            </h2>
+            <p className="text-gray-700 dark:text-gray-300 mb-4">
+            
+            </p>
+            <a
+              href=""
               target="_blank"
               rel="noopener noreferrer"
               className="text-indigo-600 dark:text-indigo-400 font-semibold hover:underline"
